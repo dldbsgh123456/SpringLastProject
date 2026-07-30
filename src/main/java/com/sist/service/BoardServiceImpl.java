acaf4f4f2ce914eaefeab3450f2209bad8d0afff
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-07-30 17:49:29 +0900

    7777777777777

---
+++ src/main/java/com/sist/service/BoardServiceImpl.java
@@ -0,0 +1,125 @@
+package com.sist.service;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sist.mapper.*;
+import com.sist.vo.*;
+
+import lombok.RequiredArgsConstructor;
+/*
+ *   Mapper : 데이터베이스 연결 
+ *     => 메뉴 
+ *     
+ *   Service : 여기의 SQL문장 조합해서 결과물 => BI(기능 통합)
+ *     => 주방 
+ *     
+ *   Controller : Service에서 제공하는 결과값을 => 브라우저로 전송 
+ *     => 서빙  
+ *     
+ *   JSP => 손님 
+ */
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService{
+    private final BoardMapper mapper; // 스프링에서 주소값 제공 
+
+	@Override
+	public List<BoardVO> boardListData(int start) {
+		// TODO Auto-generated method stub
+		return mapper.boardListData(start);
+	}
+	
+	@Override
+	public int boardRowCount() {
+		// TODO Auto-generated method stub
+		return mapper.boardRowCount();
+	}
+	
+	@Override
+	public void boardInsert(BoardVO vo) {
+		// TODO Auto-generated method stub
+	    mapper.boardInsert(vo);	
+	}
+
+	@Override
+	public BoardVO boardDetailData(int no) {
+		// TODO Auto-generated method stub
+		mapper.boardHitIncrement(no);
+		return mapper.boardDetailData(no);
+	}
+
+	@Override
+	@Transactional   // => AOP적용 
+	/*
+	 *     외부(메소드 접근전)  내부(메소드 안)
+	 *      | 인터셉트        | AOP => Transaction , Log 
+	 *      | 자동로그인 / 알림 
+	 *     public void boardReplyInsert(int pno,BoardVO vo) 
+	 *     {
+	 *        @Before => session.openSession()
+	 *        try
+	 *        {
+	 *             conn.setAutoCommit(false) => @Around
+	 *             BoardVO pvo=mapper.boardParentInfoData(pno);
+	 *             mapper.boardStepIncrement(pvo.getGroup_id(), pvo.getGroup_step());	
+	 *             mapper.boardReplyInsert(vo);
+	 *             mapper.boardDepthIncrement(pno);
+	 *             conn.commit()
+	 *        }catch(Exception e)
+	 *        {
+	 *             conn.rollback() => @AfterThrowing
+	 *        }
+	 *        finally
+	 *        {
+	 *            conn.setAutoCommit(true) => @After
+	 *        }  
+	 *     }
+	 */
+	public void boardReplyInsert(int pno,BoardVO vo) {
+		// TODO Auto-generated method stub
+		/*
+		 *   @Select("SELECT group_id,group_step,group_tab "
+		              +"FROM springReplyBoard "
+		              +"WHERE no=#{no}")
+		 */
+		BoardVO pvo=mapper.boardParentInfoData(pno);
+        /*
+         *   @Update("UPDATE springReplyBoard SET "
+		      +"group_step=group_step+1 "
+		      +"WHERE group_id=#{group_id} AND group_step>#{group_step}")
+         */
+		mapper.boardStepIncrement(pvo.getGroup_id(), pvo.getGroup_step());	
+		/*
+		 *  @Insert("INSERT INTO springReplyBoard(no,name,subject,content,pwd,group_id,group_step,group_tab,root,depth) "
+			 +"VALUES(srb_no_seq.nextval,#{name},#{subject},"
+			 +"#{content},#{pwd},"
+			 +"(SELECT NVL(MAX(group_id)+1,1) FROM springReplyBoard),#{group_step},#{group_tab},#{root},#{depth})")
+		 * 
+		 */
+		vo.setGroup_id(pvo.getGroup_id());
+		vo.setGroup_step(pvo.getGroup_step()+1);
+		vo.setGroup_tab(pvo.getGroup_tab()+1);
+		vo.setRoot(pno);
+		vo.setDepth(0);
+		mapper.boardReplyInsert(vo);
+		/*
+		 *  @Update("UPDATE springReplyBoard SET "
+		 +"depth=depth+1 "
+		 +"WHERE no=#{no}")
+		 */
+		mapper.boardDepthIncrement(pno);
+	}
+
+	@Override
+	public void boardUpdate(BoardVO vo) {
+		// TODO Auto-generated method stub
+		mapper.boardUpdate(vo);
+	}
+
+   
+}
---
+++ src/main/java/com/sist/web/BoardController.java
@@ -0,0 +1,97 @@
+package com.sist.web;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sist.vo.*;
+
+import lombok.RequiredArgsConstructor;
+
+import com.sist.service.*;
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+   private final BoardService bService;
+   
+   @GetMapping("board/list.do")
+   public String board_list(String page, Model model)
+   {
+	   if(page==null)
+		   page="1";
+	   int curpage=Integer.parseInt(page);
+	   final int ROWSIZE=10;
+	   int start=(ROWSIZE*curpage)-ROWSIZE;
+	   List<BoardVO> list=bService.boardListData(start);
+	   int count=bService.boardRowCount();
+	   int totalpage=(int)(Math.ceil(count/10.0));// 총페이지 
+	   count=count-((curpage*ROWSIZE)-ROWSIZE);
+	   /*
+	    *  count  == 20   => 20 19...
+	    *            10   => 10 9...
+	    *  
+	    */
+	   
+	   model.addAttribute("list", list);
+	   model.addAttribute("curpage", curpage);
+	   model.addAttribute("totalpage", totalpage);
+	   model.addAttribute("count", count);
+	   model.addAttribute("today", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+	   
+	   model.addAttribute("main_jsp", "../board/list.jsp");
+	   return "main/main";
+   }
+   @GetMapping("board/insert.do")
+   public String board_insert(Model model) 
+   {
+	   model.addAttribute("main_jsp", "../board/insert.jsp");
+	   return "main/main";
+   }
+   @PostMapping("board/insert_ok.do")
+   public String board_insert_ok(BoardVO vo)
+   {
+	 bService.boardInsert(vo);
+	 return "redirect:../board/list.do";   
+   }
+   @GetMapping("board/detail.do")
+   public String board_detail(int no,Model model)
+   {
+	   BoardVO vo=bService.boardDetailData(no);
+	   model.addAttribute("vo", vo);
+	   model.addAttribute("main_jsp", "../board/detail.jsp");
+	   return "main/main";
+   }
+   @GetMapping("board/reply.do")
+   public String board_reply(int no,Model model)
+   {
+	   model.addAttribute("no", no);
+	   model.addAttribute("main_jsp", "../board/reply.jsp");
+	   return "main/main";
+   }
+   @PostMapping("board/reply_ok.do")
+   public String board_reply_ok(int pno,BoardVO vo)
+   {
+	   bService.boardReplyInsert(pno, vo);
+	   return "redirect:../board/list.do";
+   }
+   
+   @GetMapping("board/update.do")
+   public String board_update(int no,Model model)
+   {
+	   BoardVO vo=bService.boardDetailData(no);
+	   model.addAttribute("vo", vo);
+	   model.addAttribute("main_jsp", "../board/update.jsp");
+	   return "main/main";
+   }
+   
+   @PostMapping("board/update_ok.do")
+   public String board_update_ok(BoardVO vo)
+   {
+	   bService.boardUpdate(vo);
+	   return "redirect:../board/list.do";
+   }
+   
+}
